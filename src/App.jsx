a8419b2e5021dae7570c280742006e9e71972c9e
Tuumix <wellz-tkn@hotmail.com>
2020-08-06 02:27:51 -0300

    botão de resetar não implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import Card from './componentes/Cards/cards'
+import React,{useState} from 'react'
+import Placar1 from './componentes/placar'
+import Placar2 from './componentes/placar'
+import { Button } from '@material-ui/core';
 
-export default () => {
-    return (
-        <Card/>
+import './App.css'
+
+export default () =>{
+    const [pontuacao,setPontos] = useState(0)
+
+    return(
+            <div className="Main">
+                <div className="grid-placar">
+                    <Placar1 color="#273c75" team="team#1" pontuacao={0}/>
+                    <Placar2 color="#3c6382" team="team#2" pontuacao={0}/>
+                </div>
+            </div>
     )
 }
