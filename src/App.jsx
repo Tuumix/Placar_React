c9c2182dad8bd6e27c0e6bc3804e77348ee58c38
Tuumix <wellz-tkn@hotmail.com>
2020-08-09 12:47:24 -0300

    textfield added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,65 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react'
 import Placar1 from './componentes/placar'
 import Placar2 from './componentes/placar'
-import { Button } from '@material-ui/core';
-
 import './App.css'
+import {TextField} from '@material-ui/core';
 
-export default () =>{
-    const [pontuacao,setPontos] = useState(0)
+export default() => {
+    const [pontuacao, setPontos] = useState(0)
+    const [team1, setTeam1] = useState('');
+    const [team2, setTeam2] = useState('');
 
-    return(
-            <div className="Main">
-                <div className="grid-placar">
-                    <Placar1 color="#273c75" team="team#1" pontuacao={0}/>
-                    <Placar2 color="#3c6382" team="team#2" pontuacao={0}/>
-                </div>
+    const styles = {
+        color: 'pink'
+    }
+
+    return (
+        <div className="Main">
+            <div className="times_nome">
+                <TextField
+                    InputLabelProps={{
+                        style: {
+                            color: "#dff9fb"
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            color: "#dff9fb"
+                        }
+                    }}
+                    inputProps={{
+                        maxLength: 20
+                    }}
+                    label="Team #1"
+                    id="filled-size-small"
+                    variant="filled"
+                    size="small"
+                    onInput={e => setTeam1(e.target.value)}/>
+                <TextField
+                    InputLabelProps={{
+                        style: {
+                            color: "#dff9fb"
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            color: "#dff9fb"
+                        }
+                    }}
+                    inputProps={{
+                        maxLength: 20
+                    }}
+                    label="Team #2"
+                    id="filled-size-small"
+                    variant="filled"
+                    size="small"
+                    maxLength="10"
+                    onInput={e => setTeam2(e.target.value)}/>
             </div>
+            <div className="grid-placar">
+                <Placar1 color="#273c75" team={team1}/>
+                <Placar2 color="#3c6382" team={team2}/>
+            </div>
+        </div>
     )
 }
--- src/componentes/placar.jsx
+++ src/componentes/placar.jsx
@@ -5,6 +5,12 @@ import { Button } from '@material-ui/core';
 export default (param) => {
     const [pontuacao,setPonto] = useState(0);
 
+    function subtrai_ponto(){
+        if(pontuacao > 0){
+            setPonto(pontuacao - 1);
+        }
+    }
+
     return (
         <div className="placar" style={{backgroundColor: param.color}}>
             <h2>{param.team}</h2>
@@ -12,7 +18,7 @@ export default (param) => {
                 <h1>{pontuacao}</h1>
             </div>
             <div className="btn-placar">
-                <Button style={{backgroundColor:'#f94144', fontSize:'60px'}} size="large" onClick={() => setPonto(pontuacao - 1)}>-</Button>
+                <Button style={{backgroundColor:'#f94144', fontSize:'60px'}} size="large" onClick={subtrai_ponto}>-</Button>
                 <Button style={{backgroundColor:'#16db93', fontSize:'40px'}} size="large" onClick={() => setPonto(pontuacao + 1)}>+</Button>
             </div>
         </div>
